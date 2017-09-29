f76105f7dfea91655ba3e47c25d771438da8bba5
Takashi Kitajima <inc@2inc.org>
2017-09-29 12:55:40 +0900

    Refactoring

--- src/app/model/parser.php
+++ src/app/model/parser.php
@@ -33,50 +33,80 @@ class Inc2734_WP_OEmbed_Blog_Card_Parser {
 
 	/**
 	 * @param string $url
-	 * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-	 * @SuppressWarnings(PHPMD.NPathComplexity)
 	 */
 	public function __construct( $url ) {
 		$this->url = $url;
 		$response  = wp_remote_get( $this->url );
-		if ( wp_http_validate_url( $url ) ) {
-			if ( is_array( $response ) && isset( $response['body'] ) ) {
-				$this->content = $response['body'];
-			}
 
-			if ( is_array( $response ) && isset( $response['response'] ) ) {
-				if ( is_array( $response['response'] ) && isset( $response['response']['code'] ) ) {
-					$this->status_code = $response['response']['code'];
-				}
-			}
-		} else {
-			if ( WP_Filesystem() ) {
-				global $wp_filesystem;
-				$this->content = $wp_filesystem->get_contents( $url );
-				if ( $this->content ) {
-					$this->status_code = 200;
-				}
-			}
-		}
+		$this->content     = $this->_get_content( $response );
+		$this->status_code = $this->_get_status_code( $response );
 
-		if ( $this->content ) {
-			if ( function_exists( 'mb_convert_encoding' ) && $this->content ) {
-				foreach ( array( 'UTF-8', 'SJIS', 'EUC-JP', 'ASCII', 'JIS' ) as $encode ) {
-					$encoded_content = mb_convert_encoding( $this->content, $encode, $encode );
-					if ( strcmp( $this->content, $encoded_content ) === 0 ) {
-						$from_encode = $encode;
-						break;
-					}
-				}
-				if ( ! empty( $from_encode ) ) {
-					$this->content = mb_convert_encoding( $this->content, get_bloginfo( 'charset' ), $from_encode );
-				}
-			}
+		if ( $this->content && ! $this->status_code ) {
+			$this->status_code = 200;
 		}
 
 		if ( ! $this->status_code ) {
 			$this->status_code = '404';
 		}
+	}
+
+	/**
+	 * @param array $response
+	 * @return string
+	 */
+	protected function _get_content( $response ) {
+		if ( wp_http_validate_url( $this->url ) ) {
+			if ( is_array( $response ) && isset( $response['body'] ) ) {
+				$content = $response['body'];
+			}
+		} elseif ( WP_Filesystem() ) {
+			global $wp_filesystem;
+			$content = $wp_filesystem->get_contents( $this->url );
+		}
+
+		if ( ! $content ) {
+			return;
+		}
+
+		return $this->_encode( $content );
+	}
+
+	/**
+	 * @param array $response
+	 * @return string
+	 */
+	protected function _get_status_code( $response ) {
+		if ( wp_http_validate_url( $this->url ) ) {
+			if ( is_array( $response ) && isset( $response['response'] ) ) {
+				if ( is_array( $response['response'] ) && isset( $response['response']['code'] ) ) {
+					return $response['response']['code'];
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param string $content
+	 * @return string
+	 */
+	protected function _encode( $content ) {
+		if ( ! function_exists( 'mb_convert_encoding' ) || ! $content ) {
+			return $content;
+		}
+
+		foreach ( array( 'UTF-8', 'SJIS', 'EUC-JP', 'ASCII', 'JIS' ) as $encode ) {
+			$encoded_content = mb_convert_encoding( $content, $encode, $encode );
+			if ( strcmp( $content, $encoded_content ) === 0 ) {
+				$from_encode = $encode;
+				break;
+			}
+		}
+
+		if ( empty( $from_encode ) ) {
+			return $content;
+		}
+
+		return mb_convert_encoding( $content, get_bloginfo( 'charset' ), $from_encode );
 	}
 
 	/**
