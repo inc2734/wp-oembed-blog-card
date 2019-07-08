9bc50d4982180a000b80a3b1a7a6794070e4a3c6
Takashi Kitajima <inc@2inc.org>
2019-07-08 15:22:01 +0900

    https://github.com/inc2734/snow-monkey/issues/582

--- src/App/Setup/Assets.php
+++ src/App/Setup/Assets.php
@@ -49,7 +49,7 @@ class Assets {
 			'wp-oembed-blog-card',
 			'WP_OEMBED_BLOG_CARD',
 			[
-				'endpoint' => site_url( '/wp-json/wp-oembed-blog-card/v1' ),
+				'endpoint' => home_url( '/wp-json/wp-oembed-blog-card/v1' ),
 			]
 		);
 	}
