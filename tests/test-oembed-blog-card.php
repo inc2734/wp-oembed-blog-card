411e7ac9b656892971c89c0675e94367fe7a4d48
inc2734 <inc@2inc.org>
2023-06-02 01:37:09 +0900

    Some fixes

--- tests/test-oembed-blog-card.php
+++ tests/test-oembed-blog-card.php
@@ -18,8 +18,8 @@ class OEmbed_Blog_Card_Test extends WP_UnitTestCase {
 		new Inc2734\WP_OEmbed_Blog_Card\Bootstrap();
 
 		$this->assertEquals(
-			'<div class="js-wp-oembed-blog-card"><a class="js-wp-oembed-blog-card__link" href="https://2inc.org" target="_blank">https://2inc.org</a></div>',
-			trim( apply_filters( 'the_content', '[embed]https://2inc.org[/embed]' ) )
+			'<div class="js-wp-oembed-blog-card"><a class="js-wp-oembed-blog-card__link" href="https://2inc.org" target="_blank"></p><div class="wp-oembed-blog-card__body"><div class="wp-oembed-blog-card__content"><div class="wp-oembed-blog-card__description">https://2inc.org</div></div></div><p></a></div>',
+			trim( str_replace( PHP_EOL, '', apply_filters( 'the_content', '[embed]https://2inc.org[/embed]' ) ) )
 		);
 	}
 
@@ -31,7 +31,7 @@ class OEmbed_Blog_Card_Test extends WP_UnitTestCase {
 
 		$this->assertEquals(
 			'<p><iframe loading="lazy" title="Snow Monkey ミートアップ 〜Snow Monkey について考える会〜" width="500" height="281" src="https://www.youtube.com/embed/s2tDDSBAHsQ?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>',
-			trim( apply_filters( 'the_content', '[embed]https://www.youtube.com/watch?v=s2tDDSBAHsQ[/embed]' ) )
+			trim( str_replace( PHP_EOL, '', apply_filters( 'the_content', '[embed]https://www.youtube.com/watch?v=s2tDDSBAHsQ[/embed]' ) ) )
 		);
 	}
 }
