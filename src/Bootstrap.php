732ac7e791d53d522a03a9013e8b9363721864ca
inc2734 <inc@2inc.org>
2024-09-03 07:45:39 +0900

    inc2734/snow-monkey#1430

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -222,6 +222,9 @@ class Bootstrap {
 	 */
 	protected function _is_block_embed_rendering_request() {
 		$reuest_uri = filter_input( INPUT_SERVER, 'REQUEST_URI' );
+		if ( ! $reuest_uri ) {
+			$reuest_uri = $_SERVER['REQUEST_URI'];
+		}
 
 		if ( ! $reuest_uri ) {
 			return false;
